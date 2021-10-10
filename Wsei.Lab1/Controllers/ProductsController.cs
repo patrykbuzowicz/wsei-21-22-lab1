f5bed02ae3c35469ad470906566acb5a5b6fae8c
Patryk Buzowicz <patrykbuzowicz@gmail.com>
2021-10-10 11:08:10 +0200

    add product controller

---
+++ Wsei.Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wsei.Lab1.Models;
+
+namespace Wsei.Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductModel product)
+        {
+            var viewModel = new ProductStatsViewModel
+            {
+                NameLength = product.Name.Length,
+                DescriptionLength = product.Description.Length,
+            };
+
+            return View(viewModel);
+        }
+    }
+}
